b24dcaf5acfa5f1f07dbe8f476ebd09be6568801
bluedabdidabdae <bluedabdidabdae@gmail.com>
2024-10-04 11:59:27 +0200

    moved some programs to other repos

--- sort_algs/headers/alg_menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALG_MENU_H
-#define ALG_MENU_H
-
-int AlgsMenuHandler(alg element_list[]);
-void RecapScreen(char algName[][40], float algtime, data element_list[]);
-void DrawWaitSortingScreen();
-
-
-#endif
